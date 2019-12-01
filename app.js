97ba2bc8117c59fcdc6fc5d48dcfcfff10bf8864
Dani Matuko <matuko305@gmail.com>
2019-12-01 14:06:52 +0200

    app is working

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ function showMovies() {
     // Get the user input
     let value = searchValue.val();
     // Make a Get request
-    $.get(`http://www.omdbapi.com/?apikey=f1f0bada&s=${value}`, function(data) {
+    $.get(`http://www.omdbapi.com/?apikey=yourAPIkey&s=${value}`, function(data) {
         const movies = data['Search'];
         //  Create element for each movie
         let output = '';
@@ -46,51 +46,40 @@ function getMovieID(movieId) {
 function showDetails() {
     let id = sessionStorage.getItem('mId');
 
-    const movie = $.get(`http://www.omdbapi.com/?apikey=f1f0bada&i=${id}`, function(data) {
+    const movie = $.get(`http://www.omdbapi.com/?apikey=yourAPIkey&i=${id}`, function(data) {
         console.log(movie.responseJSON);
         let output =
-            ` <div class="row">
+            ` <div class="row justify-content-between my-4 ">
                 <div = col-md-4>
-                    <img class="thumbnail" src="${movie.responseJSON.Poster}">
+                    <img class="card-img" src="${movie.responseJSON.Poster}">
                 </div>
-                <div class="details col-md-8 align-self-center">
-                <h2> ${movie.responseJSON.Title} </h2>
+                <div class="details col-md-8 ">
+                <h2 class = "p-1"> ${movie.responseJSON.Title} </h2>
                   <ul class="list-group">
                     <li class="list-group-item"> Actors: ${movie.responseJSON.Actors} </li>
                     <li class="list-group-item"> Director: ${movie.responseJSON.Director} </li>
                     <li class="list-group-item"> Production: ${movie.responseJSON.Production} </li>
                     <li class="list-group-item"> Released: ${movie.responseJSON.Released} </li>
                     <li class="list-group-item"> Genre: ${movie.responseJSON.Genre} </li>
-                    <li class="list-group-item"> Ratings: ${movie.responseJSON.Ratings} </li>
+                    <li class="list-group-item"> Rating: ${movie.responseJSON.imdbRating} </li>
+                    <li class="list-group-item"> Language: ${movie.responseJSON.Language} </li>
                </ul>
                 </div>
             </div>
 
-            <div class="row">
-                <div class= "well">
-                    <h3> Plot </h3>
+            <div class="row ">
+                <div class= "rounded bg-dark p-4 col-12">
+                    <h3> Plot </h3> 
+                    <hr>
                     <p class = "align-self-end">${movie.responseJSON.Plot}</p>
                 </div>
+            </div>
+
+            <div class="row justify-content-center ">
+            <a class="btn btn-primary mt-3" href= "index.html">Back To Search</a>
             </div>`
 
         // Insert HTML
         $('.container').html(output);
     })
 }
-
-/*
-        let output =
-            `<div class=" data d-flex align-items-center justify-content-around">
-            <img class="poster " src="${movie.responseJSON.Poster}">
-            <div class="details ">
-              <ul class="list-group">
-                 <li class="list-group-item"> Actors: ${movie.responseJSON.Actors} </li>
-                 <li class="list-group-item"> Director: ${movie.responseJSON.Director} </li>
-                 <li class="list-group-item"> Production: ${movie.responseJSON.Production} </li>
-                 <li class="list-group-item"> Released: ${movie.responseJSON.Released} </li>
-                 <li class="list-group-item"> Genre: ${movie.responseJSON.Genre} </li>
-                 <li class="list-group-item"> Ratings: ${movie.responseJSON.Ratings} </li>
-              </ul>
-            </div>
-         </div>`
-         */
